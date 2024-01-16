05b3a8f497fcd5dd7b70278483864e87246b77be
Vova <vovan0977967895@gmail.com>
2024-01-16 14:25:35 +0200

    sin

--- js/main.js
+++ js/main.js
@@ -1,11 +1,51 @@
-let btnArr = document.querySelectorAll('.btn');
-console.log(btnArr);
-function clickOut () {
-    let out = '';
-    for(let key in btnArr){
-        out = btnArr[key]
-        console.log(btnArr[key]);
+const display = document.querySelector('.js-dispay');
+let btns = document.querySelectorAll('.js-btn');
+let btnsCalculate = document.querySelectorAll('.calculate');
+console.log(btnsCalculate);
+console.log(btns);
 
-    }
+btns.forEach((item) => {
+item.addEventListener('click', (e) =>{
+let itemText = e.target.textContent
+if(itemText === 'x'){
+    itemText = '*';
 }
-btnArr.onclick = clickOut;
+if(itemText === '÷'){
+    itemText = '/'; 
+}
+if(itemText !== 'AC' && itemText !== 'C' &&  itemText !== '='){
+    display.value += itemText;
+}
+if(itemText === 'AC'){
+    display.value = '';
+}
+if(itemText === 'AC'){
+    display.value = '';
+}
+if(itemText === 'C'){
+    display.value = display.value.slice(0, -1);
+}
+if(itemText === '='){
+    display.value = eval(display.value);
+}
+console.log(itemText);
+});
+
+});
+
+const sin = () =>{
+display.value = Math.sin(display.value*3.1415926535/180)
+}
+
+btnsCalculate.forEach((item) => {
+    item.addEventListener('click', (e) =>{
+    let itemText = e.target.textContent
+    
+    if(itemText === 'sin'){
+        sin();
+    }
+    console.log(itemText);
+    });
+    
+    });
+    
